30a95f48e015ab45665cf77834e3f65a89b13225
repinek <137826826+repinek@users.noreply.github.com>
2025-11-23 21:44:24 +0500

    version bump

--- src/data/modPreferences.ts
+++ src/data/modPreferences.ts
@@ -1,6 +1,6 @@
 export const ModPreferences = {
-    VERSION: "2.57", // Don't forget update after changes!
-    ENV: "dev", // dev, staging, release
+    VERSION: "2.58", // Don't forget update after changes!
+    ENV: "release", // dev, staging, release
     FOR_GAME_VERSION: "21.0.1"
 };
 
