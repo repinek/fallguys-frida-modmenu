b1285b610df952b07dda4cab1fdf4a6cd37fa320
repinek <137826826+repinek@users.noreply.github.com>
2025-11-30 14:23:08 +0500

    fix(banBypass): fixed locale

--- src/modules/game/banBypass.ts
+++ src/modules/game/banBypass.ts
@@ -58,8 +58,8 @@ export class BanBypassModule extends BaseModule {
 
     private showBannedPopup(): void {
         const popupModule = ModuleManager.get(PopupManagerModule);
-        const title = I18n.t("messages.account_banned");
-        const message = I18n.t("messages.account_banned_desc");
+        const title = I18n.t("popups.ban_bypass.title");
+        const message = I18n.t("popups.ban_bypass.message");
         popupModule?.showPopup(title, message, ModalType_enum.MT_OK, OkButtonType_enum.Green);
     }
 }
