dfa2e7b26334b5da5d405f3dc5731d848014a36e
repinek <137826826+repinek@users.noreply.github.com>
2025-12-01 00:23:08 +0500

    fix(fgdebug): can't get not created fgdebug
    when menu created it's call toggleFGDebug
    no instance fgdebug at this moment, so we schedule it in awake hook

--- src/modules/visuals/fgDebug.ts
+++ src/modules/visuals/fgDebug.ts
@@ -8,6 +8,9 @@ import { UnityUtils } from "../../utils/unityUtils.js";
  *
  * Scale to 0.4 (default size is too big for mobile)
  * and enable GameObject using setActive
+ * 
+ * also we need Awake hook, since menu is loading faster than fgdebug instance in the game, and we got:
+ * [ERROR]  il2cpp: cannot get element at index 0 as the array length is 0
  */
 
 export class FGDebugModule extends BaseModule {
@@ -17,17 +20,39 @@ export class FGDebugModule extends BaseModule {
     private GvrFPS!: Il2Cpp.Class;
     private _gvrFPSInstance?: Il2Cpp.Object;
 
+    // Methods
+    private Awake!: Il2Cpp.Method;
+
+    private isToggled: boolean = false;
+    private isAwaked: boolean = false;
     private isScaled: boolean = false;
 
     public init(): void {
         this.GvrFPS = AssemblyHelper.TheMultiplayerGuys.class("GvrFPS");
+
+        this.Awake = this.GvrFPS.method<void>("Awake");
     }
 
+    public override initHooks(): void {
+        const module = this;
+
+        this.Awake.implementation = function (): void {
+            Logger.hook("GvrFPS::Awake called");
+            this.method<void>("Awake").invoke(); // <--- OnLeave
+            module.isAwaked = true;
+            if (module.isToggled)
+                module.toggleFGDebug(true);
+        }
+    }   
+
     public toggleFGDebug(value: boolean): void {
-        try {
-            this.setFGDebugScale();
-            const gameObject = this.GvrFPSInstance.method<Il2Cpp.Object>("get_gameObject").invoke();
-            gameObject.method("SetActive").invoke(value);
+        try {   
+            this.isToggled = true;
+            if (this.isAwaked) {
+                this.setFGDebugScale();
+                const gameObject = this.GvrFPSInstance.method<Il2Cpp.Object>("get_gameObject").invoke();
+                gameObject.method("SetActive").invoke(value);
+            }
         } catch (error: any) {
             Logger.errorThrow(error);
         }
