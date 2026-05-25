3e4cfafc37aa8bd074cf9c3a6275ed2a17cc79bf
repinek <137826826+repinek@users.noreply.github.com>
2026-05-25 22:57:55 +0500

    fix: use Il2cpp.perform(...).catch(reason) instead js try catch
    maybe, will refactor this project soon, I have no idea tbh when exactly

--- src/index.ts
+++ src/index.ts
@@ -20,30 +20,25 @@ import { UpdateUtils } from "./utils/UpdateUtils";
 
 class FallGuysFridaModMenu {
     static init() {
-        try {
-            Logger.debug("Script Loaded");
-            Il2Cpp.perform(() => {
-                Logger.debug("IL2CPP Loaded");
-                Logger.infoGreen(`Fall Guys Frida Mod Menu ${ModPreferences.VERSION} (${ModPreferences.ENV}), Game Version: ${Il2Cpp.application.version!}`);
-                I18n.init();
+        Logger.debug("Script Loaded");
+        Il2Cpp.perform(() => {
+            Logger.debug("IL2CPP Loaded");
+            Logger.infoGreen(`Fall Guys Frida Mod Menu ${ModPreferences.VERSION} (${ModPreferences.ENV}), Game Version: ${Il2Cpp.application.version!}`);
+            I18n.init();
 
-                // Init Unity related
-                AssemblyHelper.init();
-                UnityLogger.init();
-                UnityUtils.init();
-                ModuleManager.initAll();
-                LocalisedStrings.init();
+            // Init Unity related
+            AssemblyHelper.init();
+            UnityLogger.init();
+            UnityUtils.init();
+            ModuleManager.initAll();
+            LocalisedStrings.init();
 
-                // Init UI
-                PopupManager.init();
-                MenuBuilder.init();
+            // Init UI
+            PopupManager.init();
+            MenuBuilder.init();
 
-                UpdateUtils.checkForUpdate();
-            });
-        } catch (error: any) {
-            // TODO: log file
-            Logger.toast(`Error while loading script: ${error.message}`, 1);
-        }
+            UpdateUtils.checkForUpdate();
+        }).catch(error => Logger.error(`Failed to initialize script: ${error}`));
     }
 }
 
